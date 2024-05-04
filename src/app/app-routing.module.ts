9634f081969cf234bc199e454cad970d058bb679
Cristofher Sebastian Palomino Fano <70586827+Sebasgithub150402@users.noreply.github.com>
2024-05-03 22:02:41 -0500

    Parte Completa Metodo de Pago

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {SigninComponent} from "./public/components/auth/signin/signin.component"
 import {SignupComponent} from "./public/components/auth/signup/signup.component";
 import {HomeComponent} from "./public/pages/home/home.component";
 import {FormVenderProductoComponent} from "./ventas/pages/form-vender-producto/form-vender-producto.component";
+import {PaymentMethodComponent} from "./public/components/payment-method/payment-method.component";
 
 const routes: Routes = [
   { path:'', component: HomeComponent },
   { path:'signin', component: SigninComponent },
   { path:'signup', component: SignupComponent },
   { path:'ventas', component: FormVenderProductoComponent },
+  { path:'metodopago', component: PaymentMethodComponent}
 ];
 
 @NgModule({
