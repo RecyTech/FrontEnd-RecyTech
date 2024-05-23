fa9cd40a992617ba439fd0ffa660b803756a6704
Salvador Chamorro <u201914367@upc.edu.pe>
2024-05-23 14:59:06 -0500

    style(form-vender-producto): UI update

--- src/app/ventas/pages/form-vender-producto/form-vender-producto.component.ts
+++ src/app/ventas/pages/form-vender-producto/form-vender-producto.component.ts
@@ -3,6 +3,8 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {VentasAPIService} from "../../services/ventas-api.service";
 import {Product} from "../../model/product.entity";
 import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-form-vender-producto',
   templateUrl: './form-vender-producto.component.html',
@@ -19,8 +21,12 @@ export class FormVenderProductoComponent {
     ProductPrecioRegular: new FormControl('', Validators.required),
     ProductImagen: new FormControl('', [Validators.required, Validators.pattern('(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)')])
   });
-
-addProduct() {
+  autoResize(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  }
+  addProduct() {
     //this.ventasService.getAll().subscribe((productos:any)=>{console.log(productos)})
     // random id
     //let id = Math.floor(Math.random() * 1000);
@@ -28,22 +34,19 @@ addProduct() {
 
 
     let productName=this.miFormulario.get("productName")?.value;
-    let producDescription=this.miFormulario.get("producDescription")?.value;
+
+    let ProductPrecioDeVenta=this.miFormulario.get("productPrice")?.value;
     let productCategory=this.miFormulario.get("productCategory")?.value;
-    let ProductPrecioDeVenta=this.miFormulario.get("ProductPrecioDeVenta")?.value;
-    let ProductPrecioRegular=this.miFormulario.get("ProductPrecioRegular")?.value;
-    let ProductImagen=this.miFormulario.get("ProductImagen")?.value;
+    let ProductImagen=this.miFormulario.get("productImage")?.value;
+    let producDescription=this.miFormulario.get("productDescription")?.value;
+
     console.log(productName)
-    const product = new Product(id,productName,producDescription,productCategory,ProductPrecioDeVenta,ProductPrecioRegular,ProductImagen);
+    const product = new Product(id,productName,producDescription,productCategory,ProductPrecioDeVenta,ProductImagen);
 
     this.ventasService.create(product).subscribe((response) => {
       console.log('Group created successfully', response);
     });
-
     alert('Producto Agregado');
     this.router.navigate(['']);
-
-
-
   }
 }
