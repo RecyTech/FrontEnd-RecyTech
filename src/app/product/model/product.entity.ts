a47319da5d282d80de99da5ad1ca6c1e95e19166
Salvador Chamorro <u201914367@upc.edu.pe>
2024-06-06 21:56:54 -0500

    commit

--- src/app/product/model/product.entity.ts
+++ src/app/product/model/product.entity.ts
@@ -3,18 +3,18 @@ export class Product {
   name:string;
   description:string;
   category:string;
-  precio:number;
+  precio:string;
   imagenUrl:string;
   created_at:Date;
   slug:string;
 
 
-  constructor(id:string='',name:string='',description:string='',category:string='',precioventa:number=0, imagenUrl:string='' ,slug:string='') {
+  constructor(id:string='',name:string='',description:string='',category:string='',precio:string='', imagenUrl:string='' ,slug:string='') {
     this.id = id;
     this.name = name;
     this.description = description;
     this.category = category;
-    this.precio = precioventa;
+    this.precio = precio;
     this.imagenUrl = imagenUrl;
     this.created_at = new Date();
     this.slug = slug;
